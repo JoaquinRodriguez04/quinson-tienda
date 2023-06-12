3f0e79850b87b01ae7e68b457687e01b5b72191f
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-06-12 00:56:18 -0300

    se agrego la carpeta que contiene el carrito de compras

---
+++ prendas_pag/clothes.js
@@ -0,0 +1,654 @@
+// ararys de objetos
+
+// products
+const arrayProducts =[
+    // remeras
+    {
+        id: "remera-01",
+        title: "remera rallada",
+        img: "clothes_imgs/remeras/remera (15).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras ralladas remera rallada"
+        },
+        price: 12500,
+    },
+
+    {
+        id: "remera-02",
+        title: "scott royal",
+        img: "clothes_imgs/remeras/remera (17).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras celestes remera celeste"
+        },
+        price: 15800,
+    },
+
+    {
+        id: "remera-03",
+        title: "Sunrise Waves",
+        img: "clothes_imgs/remeras/remera (22).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras rosas remera rosa"
+        },
+        price: 11500,
+    },
+
+    {
+        id: "remera-04",
+        title: "remera clasica verde",
+        img: "clothes_imgs/remeras/remera (6).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras verdes remera verde"
+        },
+        price: 12700,
+    },
+
+    {
+        id: "remera-05",
+        title: "remera clasica marron claro",
+        img: "clothes_imgs/remeras/remera (24).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras marrones remera marrone"
+        },
+        price: 14500,
+    },
+
+    {
+        id: "remera-06",
+        title: "remera clasica negra",
+        img: "clothes_imgs/remeras/remera (8).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras negras remera negra"
+        },
+        price: 13700,
+    },
+
+    
+    {
+        id: "remera-07",
+        title: "remera negra estampa combinada",
+        img: "clothes_imgs/remeras/remera (18).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras negras remera negra"
+        },
+        price: 16750,
+    },
+    
+    {
+        id: "remera-08",
+        title: "remera gris estampa combinada",
+        img: "clothes_imgs/remeras/remera (16).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras grises remera gris"
+        },
+        price: 13200,
+    },
+        
+    {
+        id: "remera-9",
+        title: "remera blanca estampa combinada",
+        img: "clothes_imgs/remeras/remera (23).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras blancas remera blanca"
+        },
+        price: 12200,
+    },
+    
+    {
+        id: "remera-10",
+        title: "remera blanca estampa combinada",
+        img: "clothes_imgs/remeras/remera (19).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras blancas remera blanca"
+        },
+        price: 13200,
+    },
+        
+    {
+        id: "remera-11",
+        title: "remera clasica blanca",
+        img: "clothes_imgs/remeras/remera (14).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras blancas remera blanca"
+        },
+        price: 15000,
+    },
+   
+    {
+        id: "remera-12",
+        title: "remera agua marina estampa combinada",
+        img: "clothes_imgs/remeras/remera (12).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras aguas marinas remera agua marina"
+        },
+        price: 12860,
+    },
+         
+    {
+        id: "remera-13",
+        title: "remera agua marina estampa combinada",
+        img: "clothes_imgs/remeras/remera (9).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras aguas marinas remera agua marina"
+        },
+        price: 13200,
+    },
+
+    {
+        id: "remera-14",
+        title: "remera blanca estampa combinada",
+        img: "clothes_imgs/remeras/remera (7).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras blancas remera blanca"
+        },
+        price: 15500,
+    },
+
+    {
+        id: "remera-15",
+        title: "remera blanca estampa combinada",
+        img: "clothes_imgs/remeras/remera (5).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras blancas remera blanca"
+        },
+        price: 15500,
+    },
+
+    {
+        id: "remera-16",
+        title: "remera blanca estampa combinada",
+        img: "clothes_imgs/remeras/remera (10).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras blancas remera blanca"
+        },
+        price: 15780,
+    },
+
+    // camisetas
+    {
+        id: "camiseta-01",
+        title: "camiseta gris oscura",
+        img: "clothes_imgs/remeras/remera (11).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras grises oscuras remeras gris oscura"
+        },
+        price: 15470,
+    },
+
+    {
+        id: "camiseta-02",
+        title: "camiseta azul oscura",
+        img: "clothes_imgs/remeras/remera (20).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras azules oscuras remera azul oscura"
+        },
+        price: 15500,
+    },
+
+    
+    {
+        id: "camiseta-03",
+        title: "camiseta blanca",
+        img: "clothes_imgs/remeras/remera (21).png",
+        category:{
+            name:"remeras",
+            id:"remeras",
+            id2: "remeras blancas camisetas blancas remera blanca camiseta blanca"
+        },
+        price: 15500,
+    },
+
+    // buzos
+    {
+        id: "buzo-01",
+        title: "buzo gris estampado combinado",
+        img: "clothes_imgs/buzos/buzo (1).png",
+        category:{
+            name:"buzos",
+            id:"buzos",
+            id2: "buzos grises estampados"
+        },
+        price: 25200,
+    },
+    
+    {
+        id: "buzo-02",
+        title: "buzo crema estampado combinado",
+        img: "clothes_imgs/buzos/buzo (6).png",
+        category:{
+            name:"buzos",
+            id:"buzos",
+            id2: "buzos cremas estampados"
+        },
+        price: 28200,
+    },
+
+    {
+        id: "buzo-03",
+        title: "buzo gris estampado serie 8",
+        img: "clothes_imgs/buzos/buzo (7).png",
+        category:{
+            name:"buzos",
+            id:"buzos",
+            id2: "buzos grises estampados"
+        },
+        price: 25200,
+    },
+
+    {
+        id: "buzo-04",
+        title: "buzo naranja version nas",
+        img: "clothes_imgs/buzos/buzo (8).png",
+        category:{
+            name:"buzos",
+            id:"buzos",
+            id2: "buzos naranjas"
+        },
+        price: 32999,
+    },
+
+    {
+        id: "buzo-05",
+        title: "buzo clasico negro",
+        img: "clothes_imgs/buzos/buzo (9).png",
+        category:{
+            name:"buzos",
+            id:"buzos",
+            id2: "buzos negros"
+        },
+        price: 22500,
+    },
+
+    {
+        id: "buzo-06",
+        title: "buzo clasico blanco",
+        img: "clothes_imgs/buzos/buzo (10).png",
+        category:{
+            name:"buzos",
+            id:"buzos",
+            id2: "buzos negros"
+        },
+        price: 22500,
+    },
+
+    {
+        id: "buzo-07",
+        title: "buzo gris estampa combinado",
+        img: "clothes_imgs/buzos/buzo (11).png",
+        category:{
+            name:"buzos",
+            id:"buzos",
+            id2: "buzos grises estampados"
+        },
+        price: 25200,
+    },
+
+    // pantalones
+    {
+        id: "pantalon-01",
+        title: "jogger negro oversize",
+        img: "clothes_imgs/pantalones/jogger (01).png",
+        category:{
+            name:"pantalones",
+            id:"pantalones",
+            id2: "pantalon pantalones negro oversizes oversize joggers"
+        },
+        price: 30220,
+    },
+
+    {
+        id: "pantalon-02",
+        title: "jogger marron regular oversize",
+        img: "clothes_imgs/pantalones/jogger (02).png",
+        category:{
+            name:"pantalones",
+            id:"pantalones",
+            id2: "pantalon pantalones marron jogger regular oversizes oversize joggers"
+        },
+        price: 32500,
+    },
+
+    {
+        id: "pantalon-03",
+        title: "jogger pantano regular oversize",
+        img: "clothes_imgs/pantalones/jogger (03).png",
+        category:{
+            name:"pantalones",
+            id:"pantalones",
+            id2: "pantalon pantalones jogger regular oversizes oversize joggers"
+        },
+        price: 35000,
+    },
+
+    {
+        id: "pantalon-04",
+        title: "jogger negro clasico oversize",
+        img: "clothes_imgs/pantalones/jogger (04).png",
+        category:{
+            name:"pantalones",
+            id:"pantalones",
+            id2: "pantalon pantalones jogger clasico oversizes oversize joggers"
+        },
+        price: 31450,
+    },
+    
+    {
+        id: "pantalon-05",
+        title: "jogger crema regular oversize",
+        img: "clothes_imgs/pantalones/jogger (05).png",
+        category:{
+            name:"pantalones",
+            id:"pantalones",
+            id2: "pantalon pantalones jogger regular oversizes oversize joggers"
+        },
+        price: 30100,
+    },
+    
+    {
+        id: "pantalon-06",
+        title: "jogger gris clasico oversize",
+        img: "clothes_imgs/pantalones/jogger (06).png",
+        category:{
+            name:"pantalones",
+            id:"pantalones",
+            id2: "pantalon pantalones jogger clasico oversizes oversize joggers"
+        },
+        price: 33270,
+    },
+
+        
+    {
+        id: "pantalon-06",
+        title: "jogger gris clasico",
+        img: "clothes_imgs/pantalones/jogger (07).png",
+        category:{
+            name:"pantalones",
+            id:"pantalones",
+            id2: "pantalon pantalones jogger clasico oversizes oversize joggers"
+        },
+        price: 29370,
+    },
+
+];
+
+// variables
+
+const products_container = document.querySelector(".products_container");
+const icon_list = document.getElementById("list");
+const icon_cart = document.getElementById("cart");
+const navbar_list = document.getElementById("navbar_list");
+const navbar_cart = document.querySelector("#navbar_cart");
+const btn_close = document.getElementById("btn_close");
+const close_cart = document.querySelector(".close_cart");
+const categories = document.querySelectorAll(".btn_navbar");
+const title_products = document.querySelector(".title_content");
+let buttons_add = document.querySelectorAll(".product_add");
+let cartAmount = document.querySelector(".counter");
+let cartAmount2 = document.querySelector(".counter2");
+const cart_empty = document.querySelector(".cart_empty");
+const cart_center = document.querySelector(".cart_center");
+const buy_cart = document.querySelector(".buy_cart");
+let price_total = document.querySelector("#price_total");
+let cart_product_delete = document.querySelector(".cart_product_delete");
+const icons_navbar = document.querySelector(".icons_navbar");
+const formProducts = document.querySelector("#formProducts");
+let resultSearchProduct = document.querySelector(".input_seach");
+let product_js = document.querySelector(".product_js");
+let resultEmpy = document.querySelector(".resultEmpy");
+let resultText = document.querySelector(".resultText");
+let valueLinksProducuts = localStorage.getItem("value-links-products");
+
+// functions
+
+function loadproducts(chosenProducts) {
+
+    products_container.innerHTML = "";
+
+    chosenProducts.forEach(product => {
+    
+        const div = document.createElement("div");
+        div.classList.add("product_js");
+        div.innerHTML = `
+            <img src="${product.img}" class="img_product" alt="${product.title}">
+            <div class="info_product">
+                <h2 class="product_title">${product.title}</h2>
+                <p class="product_price">$${product.price}</p>
+                <a class="product_add" id="${product.id}" href="#">agregar</a>
+            </div>
+    
+        `;
+
+        products_container.append(div);
+    });
+
+    updateAddProduct();
+};
+
+loadproducts(arrayProducts);
+
+if (valueLinksProducuts != "todos") {
+    const btnProduct = arrayProducts.filter((product) => product.category.id === valueLinksProducuts);
+    loadproducts(btnProduct);
+} else{
+    loadproducts(arrayProducts);
+};
+
+categories.forEach(btn => {
+
+    btn.addEventListener("click", (e) => {
+        e.stopPropagation();
+
+        if (e.currentTarget.id != "todos") {
+            const title_category = arrayProducts.find((product) => product.category.id === e.currentTarget.id);
+            title_products.innerText = title_category.category.name;
+
+            const btnProduct = arrayProducts.filter((product) => product.category.id === e.currentTarget.id);
+            loadproducts(btnProduct);
+        } else{
+            title_products.innerText = "todos los productos";
+            loadproducts(arrayProducts);
+        };
+
+    });
+});
+
+formProducts.addEventListener("submit", searchProducts);
+
+function searchProducts(e){
+    e.preventDefault();
+    product_js = document.querySelector(".product_js");
+
+    let searhProductsValue = resultSearchProduct.value;
+
+    const productsCategory = arrayProducts.filter((product) => product.title.includes(searhProductsValue) || product.category.id2.includes(searhProductsValue));
+
+    let countResults = productsCategory.length;
+
+    console.log(productsCategory)
+
+    if (productsCategory && productsCategory.length > 0) {
+        loadproducts(productsCategory);
+        title_products.innerText = `${countResults} resultados para tu busqueda: ${searhProductsValue}`;
+    }else{
+        loadproducts(productsCategory);
+        resultEmpy.classList.remove("display_none");
+        resultText.innerText = `no se han encontrado resultados para la busqueda: ${searhProductsValue}`;
+    };
+};
+
+function updateAddProduct(){
+    buttons_add = document.querySelectorAll(".product_add");
+
+    buttons_add.forEach(btn_add => {
+        btn_add.addEventListener("click", addToCart);
+    });
+};
+
+let productsInCart;
+let productsInCartLS = JSON.parse(localStorage.getItem("products-In-Cart"));
+
+if (productsInCartLS) {
+    productsInCart = productsInCartLS;
+} else{
+    productsInCart = [];
+};
+
+function addToCart(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    let idButton = e.currentTarget.id;
+    const productAdd = arrayProducts.find(product => product.id === idButton);
+
+    if(productsInCart.some(product => product.id === idButton)){
+        const indexCart = productsInCart.findIndex((product) => product.id === idButton);
+        productsInCart[indexCart].amount++;
+    }else{
+        productAdd.amount = 1;
+        productsInCart.push(productAdd);
+    };
+
+    reloadNumberAmount();
+    localStorage.setItem("products-In-Cart", JSON.stringify(productsInCart));
+    añadirProductosAlLateral();
+    navbar_cart.classList.add("active");
+};
+
+function reloadNumberAmount(){
+    let numberAmount = productsInCart.reduce((acc,product) => acc + product.amount,0);
+    cartAmount.innerText = numberAmount;
+    cartAmount2.innerText = numberAmount;
+};
+
+function añadirProductosAlLateral(){
+
+    if (productsInCartLS && productsInCartLS.length > 0) {
+        cart_empty.classList.add("display_none");
+        cart_center.classList.remove("display_none");
+        buy_cart.classList.remove("display_none");
+
+        cart_center.innerHTML = "";
+
+        productsInCartLS.forEach(product =>{
+            const div = document.createElement("div");
+            div.classList.add("cart_product");
+            div.innerHTML = ` 
+           
+            <div class="product" id="product">
+                <img src="${product.img}" class="img_product_cart" alt="${product.title}">
+                <div class="info_product_cart">
+                    <h2 class="product_title_cart">${product.title}</h2>
+                    <p class="count_paragraph">cantidad: <span class="count_amount">${product.amount}</span></p>
+                </div>
+            </div>
+            
+            <div class="cart_product_interactive">
+                <button class="cart_product_delete" id="${product.id}"><i class="bi bi-trash3-fill"></i></button>
+                <p class="product_price">$${product.price}</p>
+            </div>
+            `;
+            cart_center.append(div);
+        });
+
+    }else{
+        cart_empty.classList.remove("display_none");
+        cart_center.classList.add("display_none");
+        buy_cart.classList.add("display_none");
+    };
+
+    reloadNumberAmount();
+    updateRemoveProduct();
+    reloadTotal();
+};
+
+añadirProductosAlLateral();
+
+function updateRemoveProduct(){
+    cart_product_delete = document.querySelectorAll(".cart_product_delete");
+    
+    cart_product_delete.forEach(btn_remove => {
+        btn_remove.addEventListener("click", removeToCart);
+    });
+};
+
+function removeToCart(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    const idButton = e.currentTarget.id;
+    const indexCart = productsInCartLS.findIndex(product => product.id === idButton);
+        
+    productsInCartLS.splice(indexCart, 1);
+    añadirProductosAlLateral();
+
+    localStorage.setItem("products-In-Cart", JSON.stringify(productsInCartLS));
+
+};
+
+function reloadTotal(){
+    const totalCalculate = productsInCartLS.reduce((acc,product) => acc + (product.price * product.amount),0);
+    price_total.innerText = `$${totalCalculate}.99`;
+};
+
+// events
+
+icon_list.addEventListener("click",() => {
+    navbar_list.classList.toggle("active");
+});
+
+btn_close.addEventListener("click",() => {
+    navbar_list.classList.remove("active");
+});
+
+icon_cart.addEventListener("click",() => {
+    navbar_cart.classList.toggle("active");
+});
+
+close_cart.addEventListener("click",() => {
+    navbar_cart.classList.remove("active");
+});
+
+document.body.addEventListener("click", (e) => {
+    // e.stopPropagation();
+
+    if (!icons_navbar.contains(e.target) && !navbar_cart.contains(e.target) && !navbar_list.contains(e.target)){
+        navbar_list.classList.remove("active");
+        navbar_cart.classList.remove("active");
+    };
+});
